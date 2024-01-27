39d95b8b894ee002608235ddb9f34831447916b4
yaelih <yael_h@yahoo.com>
2024-01-27 15:45:34 +0200

    add user model + schema and register route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import "dotenv/config";
+import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 mongoose.connect(process.env.MONGO_URI as string);
 
@@ -10,10 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello from express endpoint!"});
-})
+app.use('/api/users', userRoutes);
 
 app.listen(7000, () => {
-  console.log("server running on localhost:7000");
-})
+  console.log('server running on localhost:7000');
+});
