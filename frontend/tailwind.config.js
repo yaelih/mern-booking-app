a941582686f3558929e4219562155c35a8ad8f57
yaelih <yael_h@yahoo.com>
2024-01-28 15:53:42 +0200

    [FE] add reack-hook-form + register form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,10 @@ export default {
   theme: {
     extend: {},
     container: {
-      padding: "10rem",
+      padding: {
+        DEFAULT: "1rem",
+        md: "10rem",
+      }
     }
   },
   plugins: [],
