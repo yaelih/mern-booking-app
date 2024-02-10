700b0111b8cff460cc24117a56cb8dd98491dcc1
yaelih <yael_h@yahoo.com>
2024-02-10 21:45:36 +0200

    [FE] add facilities section to add hotel form - wip

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,6 +1,7 @@
 import { FormProvider, useForm } from 'react-hook-form';
 import DetailsSection from './DetailsSection';
 import TypeSection from './TypeSection';
+import FacilitiesSection from './FacilitiesSection';
 
 export type HotelFormData = {
   name: string;
@@ -24,6 +25,7 @@ const ManageHotelForm = () => {
       <form className="flex flex-col gap-10">
         <DetailsSection />
         <TypeSection />
+        <FacilitiesSection />
       </form>
     </FormProvider>
   );
