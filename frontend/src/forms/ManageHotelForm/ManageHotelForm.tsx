bccfddc495acd261b35b0f26bdecb7310942f7c7
yaelih <yael_h@yahoo.com>
2024-02-11 11:11:57 +0200

    [FE] add guests section to add hotel form - wip

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -2,6 +2,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import DetailsSection from './DetailsSection';
 import TypeSection from './TypeSection';
 import FacilitiesSection from './FacilitiesSection';
+import GuestsSection from './GuestsSection';
 
 export type HotelFormData = {
   name: string;
@@ -26,6 +27,7 @@ const ManageHotelForm = () => {
         <DetailsSection />
         <TypeSection />
         <FacilitiesSection />
+        <GuestsSection />
       </form>
     </FormProvider>
   );
