e7f5ecfd914c12aba271ced64403b798277bf156
yaelih <yael_h@yahoo.com>
2024-02-13 19:14:07 +0200

    move HotelType to shared folder

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
 import multer from 'multer';
 import cloudinary from 'cloudinary';
-import Hotel, { HotelType } from '../models/hotel';
-import verifyToken from '../middleware/auth';
 import { body } from 'express-validator';
+import verifyToken from '../middleware/auth';
+import Hotel from '../models/hotel';
+import { HotelType } from '../shared/types';
 
 const router = express.Router();
 
