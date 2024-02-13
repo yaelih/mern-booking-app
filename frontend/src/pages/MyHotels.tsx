757e493129ef0745b32f1d0fcadf49d06d7decd1
yaelih <yael_h@yahoo.com>
2024-02-13 19:13:13 +0200

    [FE] fix typo in link to add-hotel

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -22,7 +22,7 @@ const MyHotels = () => {
       <span className="flex justify-between">
         <h1 className="text-3xl font-bold">My Hotels</h1>
         <Link
-          to="/add=hotel"
+          to="/add-hotel"
           className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
         >
           Add Hotel
